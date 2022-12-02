2cd3677704d2c43a5468a631fb64a25aaccaf1f6
Rishi329 <117821279+Rishi329@users.noreply.github.com>
2022-12-02 16:28:36 +0530

    Add files via upload
    sum of four digits

---
+++ 4INT.C
@@ -0,0 +1,15 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+   int a,b,c,d;
+    clrscr();
+    scanf("%d",&a);
+     scanf("%d",&b);
+      scanf("%d",&c);
+	  d=a+b+c;
+
+	 printf("%d",d);
+
+getch();
+}
