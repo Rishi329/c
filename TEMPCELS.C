9e394aa658bc8434edde205139ec22a920a36c9d
Rishi329 <117821279+Rishi329@users.noreply.github.com>
2022-12-02 16:36:29 +0530

    Add files via upload
    Celsius to Fahrenheit calculator

---
+++ TEMPCELS.C
@@ -0,0 +1,14 @@
+#include<stdio.h>
+#include<conio.h>
+
+main()
+{
+clrscr();
+   float c,f;
+   printf("enter themp in celsius");
+
+   scanf("%f",&c);
+   f=c*9/5+32;
+   printf("the temp in farenhrit is: %f",f);
+   getch();
+   }
