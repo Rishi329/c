86536c8446592e145f976d15b223c13af23ea45e
Rishi329 <117821279+Rishi329@users.noreply.github.com>
2022-12-20 08:41:36 +0530

    Add files via upload
    Inverted pyramid 
    
    ********
    *******
    ******
    *****
    ****
    ***
    **
    *

---
+++ PIRAMID.C
@@ -0,0 +1,16 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+  int i,j;
+  clrscr();
+  for(i=8;i>=0;i--)
+  {
+   printf("\n");
+ {
+  for(j=0;j<=i;j++)
+  printf("*");
+  }
+  }
+  getch();
+}
