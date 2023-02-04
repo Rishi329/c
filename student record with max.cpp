9d65f6899737f9a220dc2b88914a98a9403b4234
Rishi329 <117821279+Rishi329@users.noreply.github.com>
2023-02-04 16:25:30 +0530

    student record with max mean with swap
    student record with max.cpp

--- student record with max.cpp
+++ student record with max.cpp
@@ -7,11 +7,11 @@ struct student {
 
 };
 int main() {
-	int i,j;
-	struct student s[5];
+	int k,i,j;
+	struct student s[3];
 	printf("Enter Student Record \n \n");
 
-	for(i=0; i<5; i++) {
+	for(i=0; i<3; i++) {
 		printf("Enter sName");
 		scanf("%s",&s[i].Name);
 		printf("Enter sroll");
@@ -25,7 +25,7 @@ int main() {
 
 	printf("Student Record \n \n");
 
-	for(i=0; i<5; i++) {
+	for(i=0; i<3; i++) {
 		printf("sName:%s\n",s[i].Name);
 
 		printf("sroll:%d\n",s[i].roll);
@@ -37,14 +37,45 @@ int main() {
 
 	}
 	int max=0;
-		for(i=0;i<=5;i++)
+		for(i=0;i<3;i++)
 		{
 		if( max < s[i].marks)
 			{
 			max=s[i].marks;
 			j=i;
 	}
-	printf("Roll no %d name %s max marks %f",s[j].roll,s[j].Name,s[j].marks);
+	
 }
+printf("\nRoll no %d name %s max marks %f",s[j].roll,s[j].Name,s[j].marks);
+ int mean=1000;
+ for(i=0;i<3;i++)
+ {
+ 	if(mean > s[i].marks)
+ 	{
+ 		mean = s[i].marks;
+ 	
+ 		k=i;
+	 }
+	 
+	 
+ }
+ printf("\nRoll no %d name %s min marks %f",s[k].roll, s[k].Name, s[k].marks);
+   int m;
+   
+   m=s[j].marks;
+   s[j].marks=s[k].marks;
+   s[k].marks=m;
+   
+	for(i=0; i<3; i++) {
+		printf("sName:%s\n",s[i].Name);
+
+		printf("sroll:%d\n",s[i].roll);
+
+		printf("sphone:%d\n",s[i].phone);
+
+		printf("smarks:%f\n",s[i].marks);
+}
+
+   
 	return 0;
 }
